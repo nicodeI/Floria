741c8d45479d301ec4058b785b60f3d53bb06b53
nicodeI <nurullaci-ab107@code.edu.az>
2024-12-02 13:08:41 +0400

    first commit

---
+++ Floria/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiorella.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be positive.")]
+        public decimal Price { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
